85a5ca09a23c030baf5a2c0112306fb7c3085e1e
Raphael Lima <raphael.aolima8@gmail.com>
2019-01-30 18:39:59 -0200

    feat(server): create server

---
+++ types/settings.ts
@@ -0,0 +1,11 @@
+export interface Settings {
+  environment: string;
+  database: string;
+  usernameDB: string;
+  userPasswordDB: string;
+  dbHost: string;
+  host: string;
+  serverPort: string;
+  dbPort: string;
+  dbURL: string;
+}
